d663c9de3ab3de174b41221deb48434cc96d0fc3
JarneDirken <r0882161@student.thomasmore.be>
2023-04-30 20:29:24 +0200

    popup + images

--- main.js
+++ main.js
@@ -1,23 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
   //variables
+  //nav
   const navLinks = document.querySelectorAll('.navlist a');
   const sections = document.querySelectorAll('.section');
   const btns = document.querySelectorAll('.btn');
   const header = document.querySelector('header');
   let menu = document.querySelector('#menu-icon');
   let navList = document.querySelector('.navlist');
-  const moreInfoLinks = document.querySelectorAll('.more-info-link');
-
   document.getElementById("automaticyear").innerHTML = new Date().getFullYear();
 
-  moreInfoLinks.forEach(link => {
+  // popup
+  let moreInfoLinks = document.querySelectorAll('.more-info-link');
+  let btnClose = document.querySelectorAll('.close-btn');
+  let overlays = document.querySelectorAll('.overlay');
+  let body = document.querySelector('body');
+  // open the correct popup
+  moreInfoLinks.forEach(link => { 
     link.addEventListener('click', event => {
       event.preventDefault(); // Prevent default anchor tag behavior
-      const card = event.target.closest('.row');
-      card.classList.toggle('flipped');
+      let popupId = link.getAttribute('data-popup-id');
+      let popup = document.getElementById(popupId);
+      popup.classList.toggle('active');
+      body.classList.add('popup-open');
     });
   });
+  // Close active popup on "esc" key press
+  document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    let activePopup = document.querySelector('.popup.active');
+    if (activePopup) {
+      activePopup.classList.remove('active');
+      body.classList.remove('popup-open');
+    }
+  }
+  });
+  // close on mouse click outside popup
+  overlays.forEach(overlay => {
+    overlay.addEventListener('click', () => { 
+      let activePopup = document.querySelector('.popup.active');
+      if (activePopup) {
+        activePopup.classList.remove('active');
+        body.classList.remove('popup-open');
+      }
+    });
+  });
+  // Close active popup on button press
+  btnClose.forEach(button => {
+    button.onclick = () => {
+      let activePopup = document.querySelector('.popup.active');
+      if (activePopup) {
+        activePopup.classList.remove('active');
+        body.classList.remove('popup-open');
+      }
+    }
+  });
 
+  // navbar
   menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navList.classList.toggle('open');
